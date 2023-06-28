29e869740fe7a60db1005e44bde3f3b4e726ab50
“Taylor <t4ybennett@gmail.com>
2023-06-28 17:54:42 +1200

    Add userController for user endpoints

--- friasco-api/src/api/routes/users.js
+++ friasco-api/src/api/routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { logger } = require('../../utility/logger');
+const userController = require('../controllers/usersController')
 
 const router = express.Router();
 
@@ -7,15 +8,12 @@ const router = express.Router();
 router.get('/', (req, res) => {
   logger.info('Users::GetUsers - Initiated');
   try {
-    res.json({
-      message: 'success',
-      tempFeedback: 'GetUsers',
-    });
+    res.json(userController.GetUsers());
   } catch (error) {
     res.status(400).json({
-      message: err.message,
+      message: error.message,
     });
-    logger.error(`Users::GetUsers - Failed: ${err}`);
+    logger.error(`Users::GetUsers - Failed: ${error}`);
     return;
   }
   logger.info('Users::GetUsers - Finished');
@@ -25,15 +23,12 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   logger.info('Users::GetUser - Initiated');
   try {
-    res.json({
-      message: 'success',
-      tempFeedback: 'GetUser',
-    });
+    res.json(userController.GetUser(req));
   } catch (error) {
     res.status(400).json({
-      message: err.message,
+      message: error.message,
     });
-    logger.error(`Users::GetUser - Failed: ${err}`);
+    logger.error(`Users::GetUser - Failed: ${error}`);
     return;
   }
   logger.info('Users::GetUser - Finished');
@@ -43,15 +38,12 @@ router.get('/:id', (req, res) => {
 router.post('/new', (req, res) => {
   logger.info('Users::NewUser - Initiated');
   try {
-    res.json({
-      message: 'success',
-      tempFeedback: 'NewUser',
-    });
+    res.json(userController.NewUser(req));
   } catch (error) {
     res.status(400).json({
-      message: err.message,
+      message: error.message,
     });
-    logger.error(`Users::NewUser - Failed: ${err}`);
+    logger.error(`Users::NewUser - Failed: ${error}`);
     return;
   }
   logger.info('Users::NewUser - Finished');
@@ -61,15 +53,12 @@ router.post('/new', (req, res) => {
 router.patch('/:id', (req, res) => {
   logger.info('Users::UpdateUser - Initiated');
   try {
-    res.json({
-      message: 'success',
-      tempFeedback: 'UpdateUser',
-    });
+    res.json(userController.UpdateUser(req));
   } catch (error) {
     res.status(400).json({
-      message: err.message,
+      message: error.message,
     });
-    logger.error(`Users::UpdateUser - Failed: ${err}`);
+    logger.error(`Users::UpdateUser - Failed: ${error}`);
     return;
   }
   logger.info('Users::UpdateUser - Finished');
@@ -79,15 +68,12 @@ router.patch('/:id', (req, res) => {
 router.delete('/:id', (req, res) => {
   logger.info('Users::DeleteUser - Initiated');
   try {
-    res.json({
-      message: 'success',
-      tempFeedback: 'DeleteUser',
-    });
+    res.json(userController.DeleteUser(req));
   } catch (error) {
     res.status(400).json({
-      message: err.message,
+      message: error.message,
     });
-    logger.error(`Users::DeleteUser - Failed: ${err}`);
+    logger.error(`Users::DeleteUser - Failed: ${error}`);
     return;
   }
   logger.info('Users::DeleteUser - Finished');
