bb9a34363725a36e264aa9438190578529f2fcc9
“Taylor <t4ybennett@gmail.com>
2023-07-10 18:52:26 +1200

    Refactor routes to use controller as middleware method

--- friasco-api/src/api/routes/trips.js
+++ friasco-api/src/api/routes/trips.js
@@ -1,142 +1,21 @@
 const express = require('express');
-const logger = require('../../utility/logger');
-const Trip = require('../../models/trip');
+const tripController = require('../controllers/tripController');
 
 const router = express.Router();
 
 // GetTrips
-router.get('/', async (req, res, next) => {
-  logger.info('TripsRoute::GetTrips - Initiated');
-  try {
-    const trips = await Trip.getAll();
-
-    if (trips.length > 0) {
-      logger.info('TripsRoute::GetTrips - Success');
-      res.status(200).json({
-        message: 'success',
-        trips,
-      });
-    } else {
-      logger.info('TripsRoute::GetTrips -  No trips found');
-      res.status(204).json();
-    }
-  } catch (error) {
-    logger.error(`TripsRoute::GetTrips - Failed: ${error}`);
-    next(error);
-  }
-  logger.info('TripsRoute::GetTrips - Finished');
-});
+router.get('/', tripController.getTrips);
 
 // GetTrip
-router.get('/:id', async (req, res, next) => {
-  logger.info('TripsRoute::GetTrip - Initiated');
-  try {
-    const trip = await Trip.getById(req.params.id);
-
-    if (trip) {
-      logger.info('TripsRoute::GetTrip - Success');
-      res.status(200).json({
-        message: 'success',
-        trip,
-      });
-    } else {
-      logger.info('TripsRoute::GetTrip - Trip not found');
-      res.status(404).json({
-        message: 'not found',
-      });
-    }
-  } catch (error) {
-    logger.error(`TripsRoute::GetTrip - Failed: ${error}`);
-    next(error);
-  }
-  logger.info('TripsRoute::GetTrip - Finished');
-});
+router.get('/:id', tripController.getTrip);
 
 // NewTrip
-router.post('/new', async (req, res, next) => {
-  logger.info('TripsRoute::NewTrip - Initiated');
-  try {
-    const newTripData = new Trip(null, req.body.userId, req.body.location, req.body.startDate, req.body.endDate, req.body.status, req.body.privacyStatus);
-    const createdTripId = await Trip.createNew(newTripData);
-
-    if (createdTripId) {
-      logger.info('TripsRoute::NewTrip - Success');
-      res.status(201).json({
-        message: 'success',
-        id: createdTripId,
-      });
-    } else {
-      logger.info('TripsRoute::NewTrip - Something went wrong');
-      res.status(400).json({
-        message: 'something went wrong',
-      });
-    }
-  } catch (error) {
-    logger.error(`TripsRoute::NewTrip - Failed: ${error}`);
-    next(error);
-  }
-  logger.info('TripsRoute::NewTrip - Finished');
-});
+router.post('/new', tripController.newTrip);
 
 // UpdateTrip
-router.patch('/:id', async (req, res, next) => {
-  logger.info('TripsRoute::UpdateTrip - Initiated');
-  try {
-    const updateTripData = new Trip(req.params.id, req.body.userId, req.body.location, req.body.startDate, req.body.endDate, req.body.status, req.body.privacyStatus);
-    const changes = await Trip.updateById(updateTripData);
-
-    if (changes) {
-      logger.info('TripsRoute::UpdateTrip - Success');
-      res.send({
-        message: 'success',
-        changes,
-      });
-    } else if (changes === 0) {
-      logger.info('TripsRoute::UpdateTrip - No row was changed');
-      res.status(404).json({
-        message: 'not found',
-      });
-    } else {
-      logger.info('TripsRoute::UpdateTrip - Something went wrong');
-      res.status(400).json({
-        message: 'something went wrong',
-      });
-    }
-  } catch (error) {
-    logger.error(`TripsRoute::UpdateTrip - Failed: ${error}`);
-    next(error);
-  }
-  logger.info('TripsRoute::UpdateTrip - Finished');
-});
+router.patch('/:id', tripController.updateTrip);
 
 // DeleteTrip
-router.delete('/:id', async (req, res, next) => {
-  logger.info('TripsRoute::DeleteTrip - Initiated');
-  try {
-    const changes = await Trip.deleteById(req.params.id);
-
-    if (changes) {
-      logger.info('TripsRoute::DeleteTrip - Success');
-      res.send({
-        message: 'success',
-        changes,
-      });
-    } else if (changes === 0) {
-      logger.info('TripsRoute::DeleteTrip - Trip not found');
-      res.status(404).json({
-        message: 'not found',
-      });
-    } else {
-      logger.info('TripsRoute::DeleteTrip - Something went wrong');
-      res.status(400).json({
-        message: 'something went wrong',
-      });
-    }
-  } catch (error) {
-    logger.error(`TripsRoute::DeleteTrip - Failed: ${error}`);
-    next(error);
-  }
-  logger.info('TripsRoute::DeleteTrip - Finished');
-});
+router.delete('/:id', tripController.deleteTrip);
 
 module.exports = router;
--- friasco-api/src/api/routes/users.js
+++ friasco-api/src/api/routes/users.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const logger = require('../../utility/logger');
-const User = require('../../models/user');
+const userController = require('../controllers/userController');
 
 const router = express.Router();
 
@@ -9,136 +8,18 @@ const router = express.Router();
 // But should be good enough for now to build everything else we need off of it for mvp...
 
 // GetUsers
-router.get('/', async (req, res, next) => {
-  logger.info('UsersRoute::GetUsers - Initiated');
-  try {
-    const users = await User.getAll();
-
-    if (users.length > 0) {
-      logger.info('UsersRoute::GetUsers - Success');
-      res.status(200).json({
-        message: 'success',
-        users,
-      });
-    } else {
-      logger.info('UsersRoute::GetUsers - No users found');
-      res.status(204).json();
-    }
-  } catch (error) {
-    logger.error(`UsersRoute::GetUsers - Failed: ${error}`);
-    next(error);
-  }
-  logger.info('UsersRoute::GetUsers - Finished');
-});
+router.get('/', userController.getUsers);
 
 // GetUser
-router.get('/:id', async (req, res, next) => {
-  logger.info('UsersRoute::GetUser - Initiated');
-  try {
-    const user = await User.getById(req.params.id);
-
-    if (user) {
-      logger.info('UsersRoute::GetUser - Success');
-      res.status(200).json({
-        message: 'success',
-        user,
-      });
-    } else {
-      logger.info('UsersRoute::GetUser - User not found');
-      res.status(404).json({
-        message: 'not found',
-      });
-    }
-  } catch (error) {
-    logger.error(`UsersRoute::GetUser - Failed: ${error}`);
-    next(error);
-  }
-  logger.info('UsersRoute::GetUser - Finished');
-});
+router.get('/:id', userController.getUser);
 
 // NewUser
-router.post('/new', async (req, res, next) => {
-  logger.info('UsersRoute::NewUser - Initiated');
-  try {
-    const createdUserId = await User.createNew(req.body.email, req.body.username, req.body.password);
-
-    if (createdUserId) {
-      logger.info('UsersRoute::NewUser - Success');
-      res.status(201).json({
-        message: 'success',
-        id: createdUserId,
-      });
-    } else {
-      logger.info('UsersRoute::NewUser - Something went wrong');
-      res.status(400).json({
-        message: 'something went wrong',
-      });
-    }
-  } catch (error) {
-    logger.error(`UsersRoute::NewUser - Failed: ${error}`);
-    next(error);
-  }
-  logger.info('UsersRoute::NewUser - Finished');
-});
+router.post('/new', userController.newUser);
 
 // UpdateUser
-router.patch('/:id', async (req, res, next) => {
-  logger.info('UsersRoute::UpdateUser - Initiated');
-  try {
-    const changes = await User.updateById(req.params.id, req.body.email, req.body.username, req.body.password);
-
-    if (changes) {
-      logger.info('UsersRoute::UpdateUser - Success');
-      res.send({
-        message: 'success',
-        changes,
-      });
-    } else if (changes === 0) {
-      logger.info('UsersRoute::UpdateUser - No row was changed');
-      res.status(404).json({
-        message: 'not found',
-      });
-    } else {
-      logger.info('UsersRoute::UpdateUser - Something went wrong');
-      res.status(400).json({
-        message: 'something went wrong',
-      });
-    }
-  } catch (error) {
-    logger.error(`UsersRoute::UpdateUser - Failed: ${error}`);
-    next(error);
-  }
-  logger.info('UsersRoute::UpdateUser - Finished');
-});
+router.patch('/:id', userController.updateUser);
 
 // DeleteUser
-router.delete('/:id', async (req, res, next) => {
-  logger.info('UsersRoute::DeleteUser - Initiated');
-  try {
-    const changes = await User.deleteById(req.params.id);
-
-    if (changes) {
-      logger.info('UsersRoute::DeleteUser - Success');
-      res.send({
-        message: 'success',
-        changes,
-      });
-    } else if (changes === 0) {
-      logger.info('UsersRoute::DeleteUser - User not found');
-      res.status(404).json({
-        message: 'not found',
-      });
-    } else {
-      logger.info('UsersRoute::DeleteUser - Something went wrong');
-      res.status(400).json({
-        message: 'something went wrong',
-      });
-    }
-  } catch (error) {
-    logger.error(`UsersRoute::DeleteUser - Failed: ${error}`);
-    next(error);
-  }
-  logger.info('UsersRoute::DeleteUser - Finished');
-});
+router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
