0af597fdc7258c3f2232c65f250aa598c65eb59b
“Taylor <t4ybennett@gmail.com>
2023-07-10 19:25:57 +1200

    slight refactor

--- friasco-api/src/api/index.js
+++ friasco-api/src/api/index.js
@@ -9,8 +9,8 @@ const trips = require('./routes/trips');
 const router = express.Router();
 
 // Add Routes to router
+router.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerOptions));
 router.use('/users', users);
 router.use('/trips', trips);
-router.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerOptions));
 
 module.exports = router;
