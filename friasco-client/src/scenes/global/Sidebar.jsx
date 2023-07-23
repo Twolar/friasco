ce16001ae975ce69387b29e1755285bbf5e892ee
“Taylor <t4ybennett@gmail.com>
2023-07-23 13:34:53 +1200

    sidebar tweak

--- friasco-client/src/scenes/global/Sidebar.jsx
+++ friasco-client/src/scenes/global/Sidebar.jsx
@@ -62,7 +62,7 @@ const Sidebar = () => {
             onClick={() => setIsCollapsed(!isCollapsed)}
             icon={isCollapsed ? <MenuOutlinedIcon /> : undefined}
             style={{
-              margin: '10px 0 20px 0',
+              margin: '0 0 20px 0',
               color: colors.grey[100],
             }}
           >
