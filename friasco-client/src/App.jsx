ba4b635cadc74407e63fb3e18c3671e4c1f6480d
“Taylor <t4ybennett@gmail.com>
2023-07-31 09:53:51 +1200

    Setup administrator section in sidebar + add user management page

--- friasco-client/src/App.jsx
+++ friasco-client/src/App.jsx
@@ -2,8 +2,9 @@ import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material" ;
 import { Routes, Route } from 'react-router-dom';
 import Topbar from './scenes/global/Topbar';
-import Sidebar from './scenes/global/Sidebar';
+import Sidebar from './scenes/global/Sidebar'
 import Dashboard from './scenes/dashboard';
+import Users from './scenes/admin/users';
 // import Geography from './scenes/geography';
 // import Calendar from './scenes/calendar';
 
@@ -20,6 +21,7 @@ function App() {
             <Topbar/>
             <Routes>
               <Route path ="/" element={<Dashboard />} />
+              <Route path ="/users" element={<Users />} />
               {/* <Route path ="/geography" element={<Geography />} /> */}
               {/* <Route path ="/calendar" element={<Calendar />} /> */}
             </Routes>
--- friasco-client/src/scenes/global/Sidebar.jsx
+++ friasco-client/src/scenes/global/Sidebar.jsx
@@ -147,6 +147,21 @@ const Sidebar = () => {
               selected={selected}
               setSelected={setSelected}
             />
+
+            <Typography
+              variant='h6'
+              color={colors.grey[300]}
+              sx={{ m: '15px 0 5px 20px' }}
+            >
+              Administration
+            </Typography>
+            <Item
+              title='Users'
+              to='/users'
+              icon={<PersonOutlineOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
           </Box>
         </Menu>
       </ProSidebar>
