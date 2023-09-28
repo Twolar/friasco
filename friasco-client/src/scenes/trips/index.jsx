5472cbbf1ffb41244371dfc00aaf2f2bdf7af5c3
“Taylor <t4ybennett@gmail.com>
2023-09-28 14:11:06 +0100

    Build out columns + add editmode + add action column and buttons

--- friasco-client/src/scenes/trips/index.jsx
+++ friasco-client/src/scenes/trips/index.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
-import { Box, useTheme } from "@mui/material";
+import { Box, IconButton, useTheme } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import Header from "../../components/Header";
 import { tokens } from "../../theme";
 import { fetchTrips } from "../../data/api";
+import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
+import DeleteIcon from "@mui/icons-material/Delete";
 
 const Trips = () => {
   const theme = useTheme();
@@ -22,12 +24,36 @@ const Trips = () => {
 
   const columns = [
     { field: "id", headerName: "ID" },
-    { field: "userId", headerName: "User ID" },
-    { field: "location", headerName: "Location" },
-    { field: "startDate", headerName: "Start" },
-    { field: "endDate", headerName: "End" },
-    { field: "status", headerName: "Status" },
-    { field: "privacyStatus", headerName: "Privacy" },
+    { field: "userId", headerName: "User ID", editable: true },
+    { field: "location", headerName: "Location", editable: true },
+    { field: "startDate", headerName: "Start", editable: true },
+    { field: "endDate", headerName: "End", editable: true },
+    { field: "status", headerName: "Status", editable: true },
+    { field: "privacyStatus", headerName: "Privacy", editable: true },
+    {
+      field: "",
+      headerName: "Actions",
+      renderCell: (params) => (
+        <>
+          <IconButton
+            onClick={() => alert("Editing Row: " + params.id)}
+            style={{
+              color: colors.primary[100],
+            }}
+          >
+            <EditOutlinedIcon />
+          </IconButton>
+          <IconButton
+            onClick={() => alert("Deleting Row: " + params.id)}
+            style={{
+              color: colors.redAccent[300],
+            }}
+          >
+            <DeleteIcon />
+          </IconButton>
+        </>
+      ),
+    },
   ];
 
   return (
@@ -65,7 +91,7 @@ const Trips = () => {
           },
         }}
       >
-        <DataGrid rows={trips} columns={columns} />
+        <DataGrid rows={trips} columns={columns} editMode="row" />
       </Box>
     </Box>
   );
